74a124b71d2eee4dcfad4d772ab2885a582725d2
JAEHYEON <hyon3034@naver.com>
2025-05-05 11:05:10 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,24 @@
+m, n = map(int, input().split())
+arr = [0 for i in range(n)]
+visited = [False for i in range(m+1)]
+query = list(map(int,input().split()))
+query.sort()
+
+def recur(depth,start):
+    if depth == n:
+        for j in arr:
+            print(query[j], end=' ')
+            
+        print()
+        return
+
+    for i in range(start, m):
+        if visited[i] :
+            continue
+        
+        arr[depth] = i
+        visited[i] = True
+        recur(depth + 1, i + 1)
+        visited[i] = False
+
+recur(0,0)
