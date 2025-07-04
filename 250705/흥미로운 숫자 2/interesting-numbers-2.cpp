748f3dd614ddf7a96212ae28a4b5125268e5cd2f
JAEHYEON <hyon3034@naver.com>
2025-07-05 00:02:48 +0900

    Title: 흥미로운 숫자 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250705/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+int X, Y;
+
+int main() {
+    cin >> X >> Y;
+    int ans = 0;
+
+    for(int i = X; i <= Y; i++)
+    {
+        int temp = i;
+        int num[10] = {};
+        int prev = i % 10;
+
+        while(temp != 0)
+        {
+            num[temp % 10]++;
+
+            temp = temp / 10;
+        }
+
+        int cnt = 0;
+        bool flag = false;
+
+        for (int i = 0; i < 10; i++)
+        {
+            if (num[i] == 1)
+            {
+                flag = true;
+            }
+
+            if (num[i] >= 2)
+            {
+                cnt++;
+            }
+        }
+
+        if (cnt == 1 && flag)
+            ans++;
+    }
+
+    cout << ans;
+
+    return 0;
+}
