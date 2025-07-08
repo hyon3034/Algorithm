be8d8cfc9442b03f657dc4f127c4bb343c4bed74
JAEHYEON <hyon3034@naver.com>
2025-07-08 23:31:06 +0900

    Title: 훌륭한 점프, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250708/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+int arr[100];
+
+int main() {
+    cin >> n >> k;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    cout << arr[n-k];
+
+    return 0;
+}
